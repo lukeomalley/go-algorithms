45b6c6e9b61bdb69add31cd9ed0850e5b16c5fac
Luke O'Malley <Lukeomalley95@gmail.com>
2020-08-15 19:54:21 -0400

    initial commit

---
+++ easy/branch_sums.go
@@ -0,0 +1,40 @@
+/*
+	Branch Sums
+
+  Write a function that takes in a Binary Tree and returns a list of its branch
+  sums ordered from leftmost branch sum to rightmost branch sum.
+
+  A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree
+  branch is a path of nodes in a tree that starts at the root node and ends at
+  any leaf node.
+*/
+
+package main
+
+// This is the struct of the input root. Do not edit it.
+type BinaryTree struct {
+	Value int
+	Left  *BinaryTree
+	Right *BinaryTree
+}
+
+func BranchSums(root *BinaryTree) []int {
+	sums := []int{}
+	calculateBranchSums(root, 0, &sums)
+	return sums
+}
+
+func calculateBranchSums(node *BinaryTree, runningSum int, sums *[]int) {
+	if node == nil {
+		return
+	}
+
+	runningSum += node.Value
+	if node.Left == nil && node.Right == nil {
+		*sums = append(*sums, runningSum)
+		return
+	}
+
+	calculateBranchSums(node.Left, runningSum, sums)
+	calculateBranchSums(node.Right, runningSum, sums)
+}
